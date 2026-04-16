035093210c27da93ff44e037547bc8130b261528
dhimanshruti <158125501+dhimanshruti@users.noreply.github.com>
2026-04-16 18:42:25 +0530

    fix backend structure

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "shruti@27",
-  database: "calcom"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect(err => {
   if (err) {
-    console.error("DB connection failed:", err);
+    console.error("DB Connection Failed ❌", err);
   } else {
-    console.log("MySQL Connected ✅");
+    console.log("DB Connected ✅");
   }
 });
-module.exports = db;   // ✅ VERY IMPORTANT
+
+module.exports = db;
